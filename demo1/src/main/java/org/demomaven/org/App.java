f432efd5d4aee9f4ae58fd34fb8028c0a43387bc
smartkuna <smartkuna@gmail.com>
2019-10-17 15:47:48 -0700

    1st maven project

---
+++ demo1/src/main/java/org/demomaven/org/App.java
@@ -0,0 +1,13 @@
+package org.demomaven.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
